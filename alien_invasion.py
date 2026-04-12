4d296b983d3e2754327b654843184de11624eea4
Ishan <agarwal.ishanedu@gmail.com>
2026-04-12 16:28:09 -0400

    Refactored Game loop for event and drawing

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,19 +30,26 @@ class AlienInvasion:
         This will contain the game loop
         """
         while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                    pygame.quit()
-                    sys.exit()\
-
-            # This is adding the images in order
-            # Background       
-            self.screen.blit(self.bg, (0, 0))
-            #Ship image
-            self.ship.draw()
-            pygame.display.flip()
+            self._check_events()   
+               
+            self._update_screen()
             self.clock.tick(self.settings.FPS)
+
+    def _update_screen(self):
+        # This is adding the images in order
+        # Background 
+        self.screen.blit(self.bg, (0, 0))
+
+        #Ship
+        self.ship.draw()
+        pygame.display.flip()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                pygame.quit()
+                sys.exit()
             
 
 
