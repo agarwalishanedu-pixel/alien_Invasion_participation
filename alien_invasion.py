cd68dfcc7dafbf93d55ff2ef8d12ef26d4d8c57e
Ishan <agarwal.ishanedu@gmail.com>
2026-04-19 18:08:49 -0400

    p2 - created a single row of stationary ships in fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,8 @@ import pygame
 from settings import Settings
 from ship import Ship
 from arsenal import ShipArsenal
-from alien import Alien
+#from alien import Alien
+from alien_fleet import AlienFleet
 
 # This is the game class that contains the various methods
 class AlienInvasion:
@@ -32,7 +33,8 @@ class AlienInvasion:
 
 
         self.ship = Ship(self, ShipArsenal(self))
-        self.alien = Alien(self, 10, 10)
+        self.alien_fleet = AlienFleet(self)
+        self.alien_fleet.create_fleet()
 
     def run_game(self):
         """
@@ -41,7 +43,7 @@ class AlienInvasion:
         while self.running:
             self._check_events()   
             self.ship.update()
-            self.alien.update()
+            #self.alien.update()
             self._update_screen()
             self.clock.tick(self.settings.FPS)
 
@@ -52,7 +54,7 @@ class AlienInvasion:
 
         #Ship
         self.ship.draw()
-        self.alien.draw_alien()
+        self.alien_fleet.draw()
         pygame.display.flip()
 
     def _check_events(self):
